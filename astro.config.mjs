e45b2d89c2c78123b3447ac2b640bbc6a7e57e5c
pterror <pterrorbird@gmail.com>
2025-08-18 03:08:31 +1000

    feat[files]: fix error display for missing key

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import vue from "@astrojs/vue";
 // https://astro.build/config
 export default defineConfig({
   integrations: [vue({ appEntrypoint: "/src/pages/_vue" })],
+  compressHTML: false,
   server: { allowedHosts: ["pc"] },
 });
