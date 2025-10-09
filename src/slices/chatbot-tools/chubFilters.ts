beeef0488c80c9c5a4861dc3907e5f106fddbd04
pterror <pterrorbird@gmail.com>
2025-10-09 18:49:23 +1000

    fix[chub]: fix shadow tag name regex

--- src/slices/chatbot-tools/chubFilters.ts
+++ src/slices/chatbot-tools/chubFilters.ts
@@ -1,10 +1,7 @@
 import { computed } from "vue";
-import {
-  type ChubCard,
-} from "./chub";
+import { type ChubCard } from "./chub";
 import { useQuery } from "@tanstack/vue-query";
 import { chubQueryOptions } from "./chubQuery";
-
 
 function getChubFilterValues(card: ChubCard, tag: "NSFW" | "NSFL") {
   const author = computed(() => card.fullPath.replace(/[/][\s\S]+/, ""));
@@ -12,21 +9,27 @@ function getChubFilterValues(card: ChubCard, tag: "NSFW" | "NSFL") {
   const isExplicitFiltered = computed(() => card.topics.includes(tag));
 
   const isShadowFilteredQuery = useQuery(
-    chubQueryOptions("chubGetCardsByQuery", [{
-      type: "search",
-      params: {
-        namespace: card.projectSpace,
-        nsfw: tag !== "NSFW",
-        nsfl: tag !== "NSFL",
-        //topics: card.topics.join(","),
-        include_forks: true,
-        search: card.name.replace(/[!$^&*()-+[\]'"\+~/\\]/g, ' '),
-        min_tokens: card.nTokens,
-        max_tokens: card.nTokens,
-        username: author.value.toLowerCase() !== "anonymous" ? author.value : "", // Ensure we only get cards by this author
-      },
-    }], 
-    { enabled: computed(() => !isExplicitFiltered.value) })
+    chubQueryOptions(
+      "chubGetCardsByQuery",
+      [
+        {
+          type: "search",
+          params: {
+            namespace: card.projectSpace,
+            nsfw: tag !== "NSFW",
+            nsfl: tag !== "NSFL",
+            //topics: card.topics.join(","),
+            include_forks: true,
+            search: card.name.replace(/[!$^&*()+[\]'"+~/\\-]/g, " "),
+            min_tokens: card.nTokens,
+            max_tokens: card.nTokens,
+            username:
+              author.value.toLowerCase() !== "anonymous" ? author.value : "", // Ensure we only get cards by this author
+          },
+        },
+      ],
+      { enabled: computed(() => !isExplicitFiltered.value) },
+    ),
   );
 
   const isShadowFiltered = computed(() => {
@@ -40,13 +43,30 @@ function getChubFilterValues(card: ChubCard, tag: "NSFW" | "NSFL") {
     );
   });
 
-  const isFiltered = computed(() => isExplicitFiltered.value || isShadowFiltered.value) 
+  const isFiltered = computed(
+    () => isExplicitFiltered.value || isShadowFiltered.value,
+  );
 
   return { isFiltered, isExplicitFiltered, isShadowFiltered };
 }
 
 export function getChubFilters(card: ChubCard) {
-  const { isFiltered: isNsfw, isExplicitFiltered: isExplicitNsfw, isShadowFiltered: isShadowNsfw } = getChubFilterValues(card, "NSFW");
-  const { isFiltered: isNsfl, isExplicitFiltered: isExplicitNsfl, isShadowFiltered: isShadowNsfl } = getChubFilterValues(card, "NSFL");
-  return { isNsfw, isExplicitNsfw, isShadowNsfw, isNsfl, isExplicitNsfl, isShadowNsfl };
+  const {
+    isFiltered: isNsfw,
+    isExplicitFiltered: isExplicitNsfw,
+    isShadowFiltered: isShadowNsfw,
+  } = getChubFilterValues(card, "NSFW");
+  const {
+    isFiltered: isNsfl,
+    isExplicitFiltered: isExplicitNsfl,
+    isShadowFiltered: isShadowNsfl,
+  } = getChubFilterValues(card, "NSFL");
+  return {
+    isNsfw,
+    isExplicitNsfw,
+    isShadowNsfw,
+    isNsfl,
+    isExplicitNsfl,
+    isShadowNsfl,
+  };
 }
